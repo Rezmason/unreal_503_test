af0fe159f95ccc4dd99420cca83a3a9c7cc57018
Rezmason <jeremysachs@rezmason.net>
2023-11-04 13:11:44 -0700

    I have no idea what I'm doing

---
+++ Source/PleaseWork/PleaseWork.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class PleaseWork : ModuleRules
+{
+	public PleaseWork(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
